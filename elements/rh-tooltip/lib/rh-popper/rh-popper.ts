eaf5199f08fd844ecfc0720134b98c243490bf6a
Brian Ferry <bferry@redhat.com>
2022-04-25 13:36:01 -0400

    Adding rh-popper to rh-tooltip lib file

---
+++ elements/rh-tooltip/lib/rh-popper/rh-popper.ts
@@ -0,0 +1,132 @@
+import type { Instance } from '@popperjs/core';
+import { popperGenerator } from '@popperjs/core';
+
+import { eventListeners, popperOffsets, computeStyles, applyStyles, offset, flip, preventOverflow, arrow, hide } from '@popperjs/core';
+
+/* eslint-disable no-console */
+import { LitElement, html } from 'lit';
+import { customElement, property, query } from 'lit/decorators.js';
+
+import { pfelement } from '@patternfly/pfe-core/decorators.js';
+
+import styles from './rh-popper.css';
+import { getRandomId } from '@patternfly/pfe-core/functions/random.js';
+import { Position } from './rh-popper-types';
+
+/**
+ * Wrapper for popper.js for rh-tooltip.js. 
+ * 
+ * Can be moved out of this library if we want for multiple packages. 
+ *
+ * @summary Organizes content in a contained view on the same page
+ *
+ */
+
+@customElement('rh-popper') @pfelement()
+export class RhPopper extends LitElement {
+  static readonly version = '{{version}}';
+
+  static readonly styles = [styles];
+
+  @property({ type: String, reflect: true }) position: Position = Position.TOP;
+
+  @property({ type: Boolean, reflect: true, attribute: 'is-open' }) isOpen = true;
+
+  @property({ type: Array, reflect: true }) offset = [0, 18];
+
+  private _id = `${RhPopper.name}-${getRandomId()}`;
+
+  @query('.pf-c-invoker') _invoker?: HTMLElement|null;
+  @query('.pf-c-tooltip') _tooltip?: HTMLElement|null;
+
+  private _popper?: Instance;
+
+  private createPopper = popperGenerator({
+    defaultModifiers: [eventListeners, popperOffsets, computeStyles, applyStyles, offset, flip, preventOverflow, arrow, hide],
+  });
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    this._addListeners();
+  }
+
+  firstUpdated() {
+    this._setupPopper();
+  }
+
+  show() {
+    // set the internal state to open
+    this.isOpen = true;
+    // remove the hidden attribute so that we display the tooltip in css
+    this._tooltip?.classList.remove('hidden');
+    // every time we show the tooltip we should run update on the popper
+    // instance to ensure it is properly positioned.
+    this._popper?.update();
+  }
+
+  hide() {
+    // set the internal state to open
+    this.isOpen = false;
+    // add the hidden attribute so that we hide the tooltip in css
+    this._tooltip?.classList.add('hidden');
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this._removeListeners();
+  }
+
+  render() {
+    return html`
+      <div id="invoker-id" class="pf-c-invoker" role="tooltip" tabindex="0" aria-labelledby="${this.id}">
+        <slot name="popper-invoker"></slot>
+      </div>
+      <div id="${this.id}" class="pf-c-tooltip hidden" aria-hidden=${this.isOpen ? 'false' : 'true'}>
+        <div class="pf-c-tooltip__arrow"></div>
+        <div id="content" class="pf-c-tooltip__content">
+          <slot name="popper-content"></slot>
+        </div>
+      </div>
+    `;
+  }
+
+  _addListeners() {
+    this.addEventListener('focus', this.show);
+    this.addEventListener('blur', this.hide);
+    this.addEventListener('tap', this.show);
+    this.addEventListener('click', this.show);
+    this.addEventListener('mouseenter', this.show);
+    this.addEventListener('mouseleave', this.hide);
+  }
+
+  _removeListeners() {
+    this.removeEventListener('focus', this.show);
+    this.removeEventListener('blur', this.hide);
+    this.removeEventListener('tap', this.show);
+    this.removeEventListener('click', this.show);
+    this.removeEventListener('mouseenter', this.show);
+    this.removeEventListener('mouseleave', this.hide);
+  }
+
+  _setupPopper() {
+    if (this._invoker && this._tooltip) {
+      this._popper = this.createPopper(this._invoker, this._tooltip, {
+        placement: this.position,
+        modifiers: [
+          {
+            name: 'offset',
+            options: {
+              offset: this.offset
+            }
+          }
+        ]
+      });
+    }
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'rh-popper': RhPopper;
+  }
+}
--- elements/rh-tooltip/rh-tooltip.ts
+++ elements/rh-tooltip/rh-tooltip.ts
@@ -1,14 +1,13 @@
-import type { Instance } from '@popperjs/core';
 
 /* eslint-disable no-console */
 import { LitElement, html } from 'lit';
-import { customElement, property, query } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 
 import { pfelement } from '@patternfly/pfe-core/decorators.js';
 
 import styles from './rh-tooltip.css';
-import { getRandomId } from '@patternfly/pfe-core/functions/random.js';
-import { createPopper } from './lib/createPopper';
+
+import './lib/rh-popper/rh-popper.ts';
 
 
 /**
@@ -24,96 +23,19 @@ export class RhTooltip extends LitElement {
 
   static readonly styles = [styles];
 
-  @property({ type: String, reflect: true }) position: 'top'|'left'|'right'|'bottom' = 'top';
-
-  @property({ type: Boolean, reflect: true, attribute: 'is-open' }) isOpen = true;
-
-  @property({ type: Array, reflect: true }) offset = [0, 18];
-
-  private _id = `${RhTooltip.name}-${getRandomId()}`;
-
-  @query('.pf-c-invoker') _invoker?: HTMLElement|null;
-  @query('.pf-c-tooltip') _tooltip?: HTMLElement|null;
-
-  private _popper?: Instance;
-
-  connectedCallback(): void {
-    super.connectedCallback();
-    this._addListeners();
-  }
-
-  firstUpdated() {
-    this._setupPopper();
-  }
-
-  show() {
-    // set the internal state to open
-    this.isOpen = true;
-    // remove the hidden attribute so that we display the tooltip in css
-    this._tooltip?.classList.remove('hidden');
-    // every time we show the tooltip we should run update on the popper
-    // instance to ensure it is properly positioned.
-    this._popper?.update();
-  }
-
-  hide() {
-    // set the internal state to open
-    this.isOpen = false;
-    // add the hidden attribute so that we hide the tooltip in css
-    this._tooltip?.classList.add('hidden');
-  }
-
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    this._removeListeners();
-  }
+  @property({ type: String, reflect: true }) position: 'top'|'bottom'|'left'|'right' = 'top';
 
   render() {
     return html`
-      <div id="invoker-id" class="pf-c-invoker" role="tooltip" tabindex="0" aria-labelledby="${this.id}">
-        <slot name="invoker"></slot>
-      </div>
-      <div id="${this.id}" class="pf-c-tooltip hidden" aria-hidden=${this.isOpen ? 'false' : 'true'}>
-        <div class="pf-c-tooltip__arrow"></div>
-        <div id="content" class="pf-c-tooltip__content">
+      <rh-popper position="${this.position}">
+        <div slot="popper-invoker">
+          <slot name="icon"></slot>
+        </div>
+        <div slot="popper-content">
           <slot name="content"></slot>
         </div>
-      </div>
+      </rh-popper>
     `;
-  }
-
-  _addListeners() {
-    this.addEventListener('focus', this.show);
-    this.addEventListener('blur', this.hide);
-    this.addEventListener('tap', this.show);
-    this.addEventListener('click', this.show);
-    this.addEventListener('mouseenter', this.show);
-    this.addEventListener('mouseleave', this.hide);
-  }
-
-  _removeListeners() {
-    this.removeEventListener('focus', this.show);
-    this.removeEventListener('blur', this.hide);
-    this.removeEventListener('tap', this.show);
-    this.removeEventListener('click', this.show);
-    this.removeEventListener('mouseenter', this.show);
-    this.removeEventListener('mouseleave', this.hide);
-  }
-
-  _setupPopper() {
-    if (this._invoker && this._tooltip) {
-      this._popper = createPopper(this._invoker, this._tooltip, {
-        placement: this.position,
-        modifiers: [
-          {
-            name: 'offset',
-            options: {
-              offset: this.offset
-            }
-          }
-        ]
-      });
-    }
   }
 }
 
